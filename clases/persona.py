7e035a53155eb0d6a975fcbf975a2498464deaff
Lukas Martínez <lukasmartinez@gmail.com>
2024-11-06 22:02:56 -0300

    menu

--- clases/persona.py
+++ clases/persona.py
@@ -7,8 +7,20 @@ class Persona:
   def getNombre(self):
     return self.__nombre
   
+  def getAnacimiento(self):
+    return self.__anacimiento
+  
+  def getRut(self):
+      return self.__rut
+
   def setNombre(self, nombre):
     self.__nombre = nombre
+
+  def setAnacimiento(self, anacimiento):
+    self.__anacimiento = anacimiento
+
+  def setRut(self, rut):
+    self.__rut = rut
 
   def __str__(self):
     return f'Nombre: {self.__nombre}\nAño de nacimiento: {self.__anacimiento}\nRun: {self.__rut}'
